91d18351c3dc5539ba6e0a34aa1565782ddc01c6
iamtheasad <iamtheasad@gmail.com>
2020-04-07 15:25:10 +0600

    github pages are not showing problem soloved

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -13,8 +13,7 @@ class Courses extends Component {
                     </Row>
                     <Row>
                         <Col className="mb-4" lg={3} md={6} sm={12}>
-                            <img src="https://cdn.pixabay.com/photo/2020/03/12/19/55/northern-gannet-4926108_960_720.jpg"
-                                 className="w-100" alt="Course Image"/>
+                            <img src="https://cdn.pixabay.com/photo/2020/03/12/19/55/northern-gannet-4926108_960_720.jpg" className="w-100" alt="Course Image"/>
                         </Col>
                         <Col  className="mb-4" lg={3} md={6} sm={12}>
                             <h2 className="courseTitle">Web Development</h2>
@@ -23,8 +22,7 @@ class Courses extends Component {
                             <a href="#" className="courseDetails">Details</a>
                         </Col>
                         <Col  className="mb-4" lg={3} md={6} sm={12}>
-                            <img src="https://cdn.pixabay.com/photo/2020/04/05/09/22/cherry-blossoms-5005234_960_720.jpg"
-                                 className="w-100" alt="Course Image"/>
+                            <img src="https://cdn.pixabay.com/photo/2020/04/05/09/22/cherry-blossoms-5005234_960_720.jpg" className="w-100" alt="Course Image"/>
                         </Col>
                         <Col  className="mb-4" lg={3} md={6} sm={12}>
                             <h2 className="courseTitle">Web Development</h2>
