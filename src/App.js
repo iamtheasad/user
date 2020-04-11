71b032e37b21372109931599cc4a1294e24f54e7
iamtheasad <iamtheasad@gmail.com>
2020-04-11 13:14:51 +0600

    ContactPage Component Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PortfolioPage from "./pages/PortfolioPage";
+import ContactSection from "./components/ContactSection/ContactSection";
+import ContactPage from "./pages/ContactPage";
 
 function App() {
     return (
         <div>
-          <PortfolioPage/>
+          <ContactPage/>
         </div>
     );
 }
