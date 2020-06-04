3e4d5f3662e83a9a8f9b5a9b22f38884a0107418
iamtheasad <iamtheasad@gmail.com>
2020-06-04 15:55:58 +0600

    TopBanner Component title & subtitle content called from database with axios

--- src/components/TopBanner/TopBanner.js
+++ src/components/TopBanner/TopBanner.js
@@ -1,13 +1,28 @@
 import React, {Component, Fragment} from 'react';
-
 import {Button, Col, Container, Row} from "react-bootstrap";
 import AppUrl from "../../RestAPI/AppUrl";
 import RestClient from "../../RestAPI/RestClient";
 
+
 class TopBanner extends Component {
 
+    constructor() {
+        super();
+
+        this.state = {
+            title: '',
+            subtitle: ''
+        }
+    }
+
     componentDidMount() {
-        RestClient.GetRequest(AppUrl.HomeTopTitle);
+        RestClient.GetRequest(AppUrl.HomeTopTitle).then(result => {
+
+            this.setState({
+                title: result[0]['home_title'],
+                subtitle: result[0]['home_subtitle']
+            })
+        });
     }
 
     render() {
@@ -18,8 +33,8 @@ class TopBanner extends Component {
                         <div className="topContent">
                             <Row>
                                 <Col className="text-center">
-                                    <h1 className="topTitle">SOFTWARE ENGINEER</h1>
-                                    <h4 className="topSubTitle">Mobile & Web Application</h4>
+                                    <h1 className="topTitle">{this.state.title}</h1>
+                                    <h4 className="topSubTitle">{this.state.subtitle}</h4>
                                     <Button variant="primary">More Info</Button>
                                 </Col>
                             </Row>
