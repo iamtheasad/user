81a6d9c93d3ee0a71eacaada5685f79c6c2d7acf
iamtheasad <iamtheasad@gmail.com>
2020-06-23 15:39:49 +0600

    api-consumed for TopBanner component

--- src/components/TopBanner/TopBanner.js
+++ src/components/TopBanner/TopBanner.js
@@ -1,8 +1,31 @@
 import React, {Component, Fragment} from 'react';
 import {Button, Col, Container, Row} from "react-bootstrap";
+import RestClient from "../../RestAPI/RestClient";
+import AppUrl from "../../RestAPI/AppUrl";
 
 
 class TopBanner extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            title: '...',
+            subtitle: '...'
+        }
+    }
+
+    componentDidMount() {
+        RestClient.GetRequest(AppUrl.HomeTopTitle).then(result => {
+            this.setState({
+                title: result[0]['home_title'],
+                subtitle: result[0]['home_subtitle']
+            })
+        }).catch(error=>{
+            this.setState({title: 'error', subtitle: 'error'})
+        })
+    }
+
     render() {
         return (
             <Fragment>
@@ -11,8 +34,8 @@ class TopBanner extends Component {
                         <div className="topContent">
                             <Row>
                                 <Col className="text-center">
-                                    <h1 className="topTitle">SOFTWARE ENGINEER</h1>
-                                    <h4 className="topSubTitle">Mobile & Web Application</h4>
+                                    <h1 className="topTitle">{this.state.title}</h1>
+                                    <h4 className="topSubTitle">{this.state.subtitle}</h4>
                                     <Button variant="primary">More Info</Button>
                                 </Col>
                             </Row>
