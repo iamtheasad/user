871818b9e32749f5aeb19bc72567c3b461486cab
iamtheasad <iamtheasad@gmail.com>
2020-07-06 11:58:52 +0600

    api-consuming for Video Component

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -4,18 +4,31 @@ import {faPlayCircle} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {Player, BigPlayButton} from "video-react";
 import "video-react/dist/video-react.css";
+import RestClient from "../../RestAPI/RestClient";
+import AppUrl from "../../RestAPI/AppUrl";
 
 class Video extends Component {
-    constructor(){
+    constructor() {
         super();
 
-        this.state={
-            show: false
+        this.state = {
+            show: false,
+            video_description: '',
+            video_url: ''
         }
     }
 
-    modalClose=()=>this.setState({show: false});
-    modalOpen=()=>this.setState({show: true});
+    componentDidMount() {
+        RestClient.GetRequest(AppUrl.VideoHome).then(result => {
+            this.setState({
+                video_description: result[0]['video_description'],
+                video_url: result[0]['video_url']
+            })
+        })
+    }
+
+    modalClose = () => this.setState({show: false});
+    modalOpen = () => this.setState({show: true});
 
     render() {
         return (
@@ -24,9 +37,7 @@ class Video extends Component {
                     <Row className="video_section">
                         <Col lg={12} className="text-center">
                             <h2 className="videoTitle">How I Do</h2>
-                            <p className="videoDes">First i analysis the requirement of project. According to the requirement i make a proper technical analysis, then i build a software architecture. According to the planning i
-                                start coding. Testing is also going on with coding. Final testing take place after
-                                finishing coding part. After successful implementation i provide 6 month free bug fixing  service for corresponding project.</p>
+                            <p className="videoDes">{this.state.video_description}</p>
                             <p>
                                 <FontAwesomeIcon icon={faPlayCircle} onClick={this.modalOpen} className="playBtn"/>
                             </p>
@@ -41,8 +52,8 @@ class Video extends Component {
                     </Modal.Header>
                     <Modal.Body>
                         <Player>
-                            <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
-                            <BigPlayButton position="center" />
+                            <source src={this.state.video_url}/>
+                            <BigPlayButton position="center"/>
                         </Player>
                     </Modal.Body>
                     <Modal.Footer>
