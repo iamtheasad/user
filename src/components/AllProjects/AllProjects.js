82761bbf75ae262433feecad9ee6af64bda754e5
iamtheasad <iamtheasad@gmail.com>
2020-08-19 16:20:52 +0600

    Loader Component added on AllProject Component

--- src/components/AllProjects/AllProjects.js
+++ src/components/AllProjects/AllProjects.js
@@ -3,6 +3,7 @@ import {Button, Card, Col, Container, Row} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import RestClient from "../../RestAPI/RestClient";
 import AppUrl from "../../RestAPI/AppUrl";
+import Loader from "../Loader/Loader";
 
 class AllProjects extends Component {
 
@@ -10,49 +11,56 @@ class AllProjects extends Component {
         super();
 
         this.state = {
-            myData: []
+            myData: [],
+            loading: true
         }
     }
 
     componentDidMount() {
         RestClient.GetRequest(AppUrl.ProjectAll).then(result => {
             this.setState({
-                myData: result
+                myData: result,
+                loading: false
             })
         })
     }
 
     render() {
 
-        const myList = this.state.myData;
-        const myView = myList.map(myList => {
-            return (
-                <Col sm={12} md={6} lg={4}>
-                    <Card className="projectCard">
-                        <Card.Img variant="top" src={myList.img_one}/>
-                        <Card.Body>
-                            <Card.Title className="projectCardTitle">{myList.project_name}</Card.Title>
-                            <Card.Text className="projectCardDes">
-                                {myList.short_description}
-                            </Card.Text>
-                            <Button variant="primary">
-                                <Link className="lind-style" to={"/projectDetails/"+myList.id+"/"+myList.project_name}>Details</Link>
-                            </Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            )
-        })
+        if (this.state.loading == true) {
+            return (<Loader/>)
+        } else {
+            const myList = this.state.myData;
+            const myView = myList.map(myList => {
+                return (
+                    <Col sm={12} md={6} lg={4}>
+                        <Card className="projectCard">
+                            <Card.Img variant="top" src={myList.img_one}/>
+                            <Card.Body>
+                                <Card.Title className="projectCardTitle">{myList.project_name}</Card.Title>
+                                <Card.Text className="projectCardDes">
+                                    {myList.short_description}
+                                </Card.Text>
+                                <Button variant="primary">
+                                    <Link className="lind-style"
+                                          to={"/projectDetails/" + myList.id + "/" + myList.project_name}>Details</Link>
+                                </Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                )
+            })
 
-        return (
-            <Fragment>
-                <Container className="text-center mt-5">
-                    <Row>
-                        {myView}
-                    </Row>
-                </Container>
-            </Fragment>
-        );
+            return (
+                <Fragment>
+                    <Container className="text-center mt-5">
+                        <Row>
+                            {myView}
+                        </Row>
+                    </Container>
+                </Fragment>
+            );
+        }
     }
 }
 
