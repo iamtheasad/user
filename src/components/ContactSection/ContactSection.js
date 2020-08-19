63ca963ed6570810d78d6f92e760166ae12737ee
iamtheasad <iamtheasad@gmail.com>
2020-08-19 16:28:32 +0600

    Loader Component added on ContactSection

--- src/components/ContactSection/ContactSection.js
+++ src/components/ContactSection/ContactSection.js
@@ -4,6 +4,7 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faEnvelope, faPhone} from "@fortawesome/free-solid-svg-icons";
 import RestClient from '../../RestAPI/RestClient';
 import AppUrl from '../../RestAPI/AppUrl';
+import Loader from "../Loader/Loader";
 
 class ContactSection extends Component {
 
@@ -14,6 +15,7 @@ class ContactSection extends Component {
             address: '...',
             email: '...',
             phone: '...',
+            loading: true
         }
     }
 
@@ -22,7 +24,8 @@ class ContactSection extends Component {
             this.setState({
                 address: result[0]['address'],
                 email: result[0]['email'],
-                phone: result[0]['phone']
+                phone: result[0]['phone'],
+                loading: false
             })
         })
     }
@@ -32,55 +35,59 @@ class ContactSection extends Component {
         let email = document.getElementById("email").value;
         let msg = document.getElementById("msg").value;
 
-       let jsonObject = {name:name,email:email,msg:msg}
+        let jsonObject = {name: name, email: email, msg: msg}
 
-       RestClient.PostRequest(AppUrl.ContactSend,JSON.stringify(jsonObject)).then(result => {
-           alert (result);
-       }).catch(error => {
-           alert("Error");
-       })
+        RestClient.PostRequest(AppUrl.ContactSend, JSON.stringify(jsonObject)).then(result => {
+            alert(result);
+        }).catch(error => {
+            alert("Error");
+        })
     }
 
 
     render() {
-        return (
-            <Fragment>
-                <Container className="mt-5">
-                    <Row>
-                        <Col lg={6} md={6} sm={12}>
-                            <h1 className="serviceName">Quick Connect</h1>
+        if (this.state.loading == true) {
+            return (<Loader/>);
+        } else {
+            return (
+                <Fragment>
+                    <Container className="mt-5">
+                        <Row>
+                            <Col lg={6} md={6} sm={12}>
+                                <h1 className="serviceName">Quick Connect</h1>
 
-                            <Form>
-                                <Form.Group>
-                                    <Form.Label className="serviceDescription">Name</Form.Label>
-                                    <Form.Control id="name" type="text"/>
-                                </Form.Group>
-                                <Form.Group>
-                                    <Form.Label className="serviceDescription">Email address</Form.Label>
-                                    <Form.Control id="email" type="email"/>
-                                </Form.Group>
-                                <Form.Group>
-                                    <Form.Label className="serviceDescription">Message</Form.Label>
-                                    <Form.Control id="msg" as="textarea" rows="5"/>
-                                </Form.Group>
+                                <Form>
+                                    <Form.Group>
+                                        <Form.Label className="serviceDescription">Name</Form.Label>
+                                        <Form.Control id="name" type="text"/>
+                                    </Form.Group>
+                                    <Form.Group>
+                                        <Form.Label className="serviceDescription">Email address</Form.Label>
+                                        <Form.Control id="email" type="email"/>
+                                    </Form.Group>
+                                    <Form.Group>
+                                        <Form.Label className="serviceDescription">Message</Form.Label>
+                                        <Form.Control id="msg" as="textarea" rows="5"/>
+                                    </Form.Group>
 
-                                <Button onClick={this.contactSend} variant="primary">
-                                    Submit
-                                </Button>
-                            </Form>
-                        </Col>
-                        <Col lg={6} md={6} sm={12}>
-                            <h1 className="serviceName">Discuss Now</h1>
-                            <p className="serviceDescription">{this.state.address}</p>
-                            <p className="serviceDescription"><span className="mr-1"><FontAwesomeIcon
-                                icon={faEnvelope}/></span> {this.state.email}</p>
-                            <p className="serviceDescription"><span className="mr-1"><FontAwesomeIcon
-                                icon={faPhone}/></span> {this.state.phone}</p>
-                        </Col>
-                    </Row>
-                </Container>
-            </Fragment>
-        );
+                                    <Button onClick={this.contactSend} variant="primary">
+                                        Submit
+                                    </Button>
+                                </Form>
+                            </Col>
+                            <Col lg={6} md={6} sm={12}>
+                                <h1 className="serviceName">Discuss Now</h1>
+                                <p className="serviceDescription">{this.state.address}</p>
+                                <p className="serviceDescription"><span className="mr-1"><FontAwesomeIcon
+                                    icon={faEnvelope}/></span> {this.state.email}</p>
+                                <p className="serviceDescription"><span className="mr-1"><FontAwesomeIcon
+                                    icon={faPhone}/></span> {this.state.phone}</p>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Fragment>
+            );
+        }
     }
 }
 
