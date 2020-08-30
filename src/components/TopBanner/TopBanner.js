960ddff0666fd73fc8b7cc4f3a7abb0352a37f4a
iamtheasad <iamtheasad@gmail.com>
2020-08-30 11:58:51 +0600

    api-failure error message added on TopBanner Component & Services Component's image link added on database

--- src/components/TopBanner/TopBanner.js
+++ src/components/TopBanner/TopBanner.js
@@ -3,6 +3,7 @@ import {Button, Col, Container, Row} from "react-bootstrap";
 import RestClient from "../../RestAPI/RestClient";
 import AppUrl from "../../RestAPI/AppUrl";
 import Loader from "../Loader/Loader";
+import WentWrong from "../WentWrong/WentWrong";
 
 
 class TopBanner extends Component {
@@ -14,21 +15,34 @@ class TopBanner extends Component {
             title: '',
             subtitle: '',
             loaderClass: 'd-block',
-            mainDivClass: 'd-none'
+            mainDivClass: 'd-none',
+            WentWrong: 'd-none'
         }
     }
 
     componentDidMount() {
         RestClient.GetRequest(AppUrl.HomeTopTitle).then(result => {
+            if (result == null) {
+                this.setState({
+                    WentWrong: 'd-block',
+                    loaderClass: 'd-none',
+                    mainDivClass: 'd-none'
+                });
+            } else {
+                this.setState({
+                    title: result[0]['home_title'],
+                    subtitle: result[0]['home_subtitle'],
+                    loaderClass: 'd-none',
+                    mainDivClass: 'd-block, text-center'
+                });
+            }
+        }).catch(error => {
             this.setState({
-                title: result[0]['home_title'],
-                subtitle: result[0]['home_subtitle'],
+                WentWrong: 'd-block',
                 loaderClass: 'd-none',
-                mainDivClass: 'd-block, text-center'
+                mainDivClass: 'd-none'
             })
-        }).catch(error=>{
-            this.setState({title: 'error', subtitle: 'error'})
-        })
+        });
     }
 
     render() {
@@ -38,9 +52,12 @@ class TopBanner extends Component {
                     <div className="topBannerOverlay">
                         <div className="topContent">
                             <Row>
-                                <Col className={this.state.loaderClass}>
-                                   <Loader/>
+                                <Col className={this.state.WentWrong}>
+                                    <WentWrong/>
                                 </Col>
+                                <Col className={this.state.loaderClass}>
+                                <Loader/>
+                            </Col>
                                 <Col className={this.state.mainDivClass}>
                                     <h1 className="topTitle">{this.state.title}</h1>
                                     <h4 className="topSubTitle">{this.state.subtitle}</h4>
