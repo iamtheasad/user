76543f52d59156a9979ca4cfabf446584eb0d1cf
iamtheasad <iamtheasad@gmail.com>
2020-09-02 10:36:32 +0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/user/precache-manifest.328d78fbff798764426b402817689932.js"
+  "/user/precache-manifest.9f9a01631fa90a78fab2f94939aa080c.js"
 );
 
 self.addEventListener('message', (event) => {
